28318826d276db342110b56f9f54ceb183f836e9
OhmPatil <workpatil10@gmail.com>
2022-06-05 12:20:53 +0530

    Starting fresh

---
+++ main.js
@@ -0,0 +1,12 @@
+let currentNumber = ''
+let previousNumber = ''
+let operator = ''
+const current_display = document.querySelector('.currentNumber')
+const previous_display = document.querySelector('.previousNumber')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operators = document.querySelectorAll('#op')
+const allClearButton = document.querySelector('#all-clear')
+const clearButton = document.querySelector('#clear')
+const equalsButton = document.querySelector('#equals')
+
+
